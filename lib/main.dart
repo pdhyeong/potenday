d730800c6c1c8f1990c5d42b963ad9dcaed6605b
Dohyeong Park <slstls218@gmail.com>
2023-12-11 17:47:58 +0900

    implementation of  SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:potenday/screen/home_screen.dart';
+import 'package:potenday/screen/splash_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,105 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  Future<void> loginUser() async {
-    final response = await http.post(
-      Uri.parse('http://localhost:3000/api/login'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'username': usernameController.text,
-        'password': passwordController.text,
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      final responseData = jsonDecode(response.body);
-      if (responseData['success']) {
-        // 로그인 성공
-        print(responseData['message']);
-      } else {
-        // 로그인 실패
-        print(responseData['message']);
-      }
-    } else {
-      // 서버 에러
-      print('Failed to connect to the server');
-    }
-  }
-
-  Future<void> signupUser() async {
-    final response = await http.post(
-      Uri.parse('http://localhost:3000/api/signup'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'username': usernameController.text,
-        'password': passwordController.text,
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      final responseData = jsonDecode(response.body);
-      if (responseData['success']) {
-        // 회원가입 성공
-        print(responseData['message']);
-      } else {
-        // 회원가입 실패
-        print(responseData['message']);
-      }
-    } else {
-      // 서버 에러
-      print('Failed to connect to the server');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('example'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: loginUser,
-              child: const Text('Login'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: signupUser,
-              child: const Text('Signup'),
-            ),
-          ],
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,101 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomeScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> loginUser() async {
+    final response = await http.post(
+      Uri.parse('http://localhost:3000/api/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': usernameController.text,
+        'password': passwordController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      if (responseData['success']) {
+        // 로그인 성공
+        print(responseData['message']);
+      } else {
+        // 로그인 실패
+        print(responseData['message']);
+      }
+    } else {
+      // 서버 에러
+      print('Failed to connect to the server');
+    }
+  }
+
+  Future<void> signupUser() async {
+    final response = await http.post(
+      Uri.parse('http://localhost:3000/api/signup'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': usernameController.text,
+        'password': passwordController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      if (responseData['success']) {
+        // 회원가입 성공
+        print(responseData['message']);
+      } else {
+        // 회원가입 실패
+        print(responseData['message']);
+      }
+    } else {
+      // 서버 에러
+      print('Failed to connect to the server');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: loginUser,
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: signupUser,
+              child: const Text('Signup'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
