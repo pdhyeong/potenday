af0dcaf8eda7f8235efbea1aa24a9dacbe8aea27
Dohyeong Park <slstls218@gmail.com>
2023-12-12 20:49:28 +0900

    android kakaologin

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@ import 'package:http/http.dart' as http;
 import 'package:potenday/screen/DatePick_screen.dart';
 import 'package:potenday/screen/splash_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  KakaoSdk.init(nativeAppKey: "875118c1635e45b43f981cfa2fe8700f");
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
-
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
@@ -10,6 +10,16 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  void _get_user_info() async {
+    try {
+      User user = await UserApi.instance.me();
+      print('사용자 정보 요청 성공'
+          '\n회원번호: ${user.id}'
+          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}');
+    } catch (error) {
+      print('사용자 정보 요청 실패 $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,23 +47,45 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             Align(
-              alignment: const AlignmentDirectional(0, 0),
+              alignment: const AlignmentDirectional(0, 0.4),
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
+                  backgroundColor: Colors.yellow,
                   side: const BorderSide(color: Colors.black26),
-                  fixedSize: const Size(95, 50),
+                  fixedSize: const Size(400, 50),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => const LoginScreen(),
-                  ));
+                onPressed: () async {
+                  if (await isKakaoTalkInstalled()) {
+                    try {
+                      await UserApi.instance.loginWithKakaoTalk();
+                      print('카카오톡으로 로그인 성공');
+                      _get_user_info();
+                    } catch (error) {
+                      print('카카오톡으로 로그인 실패 $error');
+                      // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+                      try {
+                        await UserApi.instance.loginWithKakaoAccount();
+                        print('카카오계정으로 로그인 성공');
+                        _get_user_info();
+                      } catch (error) {
+                        print('카카오계정으로 로그인 실패 $error');
+                      }
+                    }
+                  } else {
+                    try {
+                      await UserApi.instance.loginWithKakaoAccount();
+                      print('카카오계정으로 로그인 성공');
+                      _get_user_info();
+                    } catch (error) {
+                      print('카카오계정으로 로그인 실패 $error');
+                    }
+                  }
                 },
-                child: const Text('건너뛰기',
+                child: const Text('카카오 로그인',
                     style: TextStyle(
                       fontSize: 12,
                       fontWeight: FontWeight.w400,
