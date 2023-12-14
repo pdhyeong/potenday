008cffec8ff63d365db639ddaaef963a085a2b7e
SimSimEEE <smileteeth14@gmail.com>
2023-12-15 01:48:41 +0900

    Update Reason_screen.dart

--- lib/screen/Reason_screen.dart
+++ lib/screen/Reason_screen.dart
@@ -1,7 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:potenday/screen/SubReason_screen.dart';
+import 'package:potenday/main.dart';
 
 class ReasonScreen extends StatelessWidget {
-  const ReasonScreen({super.key});
+  const ReasonScreen({Key? key}) : super(key: key);
+
+  void _navigateToReasonScreen(BuildContext context, String target) {
+    GlobalStore globalStore = Provider.of<GlobalStore>(context, listen: false);
+    globalStore.arr[4] = target;
+    print(globalStore.arr);
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) => const SubReasonScreen(),
+    ));
+  }
+
+  void _showInputDialog(BuildContext context) {
+    TextEditingController textEditingController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('기타'),
+        content: TextField(
+          controller: textEditingController,
+          decoration: const InputDecoration(labelText: '기타 입력'),
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              String enteredText = textEditingController.text.trim();
+              if (enteredText.isNotEmpty) {
+                GlobalStore globalStore =
+                    Provider.of<GlobalStore>(context, listen: false);
+                globalStore.arr[3] = enteredText;
+                print(globalStore.arr);
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (_) => const ReasonScreen(),
+                ));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('입력된 텍스트가 없습니다.'),
+                  ),
+                );
+              }
+            },
+            child: const Text('저장'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildElevatedButton(BuildContext context, String label, String target,
+      double verticalPosition) {
+    return Align(
+      alignment: AlignmentDirectional(0.00, verticalPosition),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          fixedSize: const Size(361, 45),
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(width: 1, color: Colors.black),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        onPressed: label == '기타'
+            ? () => _showInputDialog(context)
+            : () => _navigateToReasonScreen(context, target),
+        child: Text(
+          label,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,90 +95,13 @@ class ReasonScreen extends StatelessWidget {
                 height: 50,
               ),
             ),
-            Align(
-              alignment: const AlignmentDirectional(0.00, 0.4),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(361, 45),
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(width: 1, color: Colors.black),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('💙일상',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                    )),
-              ),
-            ),
-            Align(
-              alignment: const AlignmentDirectional(0.00, 0.55),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(361, 45),
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(width: 1, color: Colors.black),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('😷건강',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                    )),
-              ),
-            ),
-            Align(
-              alignment: const AlignmentDirectional(0.00, 0.7),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(361, 45),
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(width: 1, color: Colors.black),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('🎄연말',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                    )),
-              ),
-            ),
-            Align(
-              alignment: const AlignmentDirectional(0.00, 0.85),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(361, 45),
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(width: 1, color: Colors.black),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('☀️ 연초',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                    )),
-              ),
-            ),
+            for (var i = 0; i < options.length; i++)
+              _buildElevatedButton(
+                  context, options[i].label, options[i].target, 0.15 * (i + 1)),
             const Align(
               alignment: AlignmentDirectional(-0.70, -0.40),
               child: Text(
-                '어떤 안부 연락을 드릴까요?',
+                '무슨 이유로 메시지를 보내시나요?',
                 style: TextStyle(
                   fontSize: 25,
                   fontWeight: FontWeight.w600,
@@ -120,3 +122,18 @@ class ReasonScreen extends StatelessWidget {
     );
   }
 }
+
+class Option {
+  final String label;
+  final String target;
+
+  const Option({required this.label, required this.target});
+}
+
+final List<Option> options = [
+  const Option(label: '👋안부', target: '👋안부'),
+  const Option(label: '🎉 축하', target: '🎉 축하'),
+  const Option(label: '🙏 부탁 ', target: '🙏 부탁 '),
+  const Option(label: '❓질문', target: '❓질문'),
+  const Option(label: '기타', target: '기타'),
+];
